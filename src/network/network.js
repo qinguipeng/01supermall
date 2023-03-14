22891a0f94c52091c1d3efe0f60c75beb1679bab
qinguipeng <1612702791@qq.com>
2023-03-14 11:54:37 +0800

    update request's baseURL

--- src/network/network.js
+++ src/network/network.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // 请求相关
 export function request(config) {
     const instance = axios.create({
-        baseURL: "http://152.136.185.210:7878/api/hy66",
+        baseURL: "http://123.207.32.32:7888/api/hy66",
         timeout: 5000
     })
 
